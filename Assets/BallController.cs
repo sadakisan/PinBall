720239bacd64f26df62d7a9e78c73408129512c9
sadaki244 <sadaki.m244@gmail.com>
2019-03-28 21:50:39 +0900

    PinBall 課題です。 ご確認宜しくお願いします。

---
+++ Assets/BallController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+
+    public GameObject scoreText;
+    private int score;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.scoreText = GameObject.Find("scoreText");
+
+        score = 0;
+        SetScore();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+
+        if (yourTag == "LargeStarTag")
+        {
+            score += 100;
+        }
+        else if (yourTag == "LargeCloudTag")
+        {
+            score += 200;
+        }
+
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        this.scoreText.GetComponent<Text>().text = string.Format("Score:{0}", score);
+    }
+}
---
+++ Assets/LargeCloud.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeCloud : MonoBehaviour {
+
+    public int scorePoint;
+
+
+	void OnCollisionEnter(Collision collision)
+    {
+        Manager.score += scorePoint;
+		
+	}
+}
